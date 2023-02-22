2828ec2e58b5b32e28419941c2cf7b625a8fc17b
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-02-22 16:41:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Exercise 1 //
+for (let i = 4; i <= 15; i++) {
+   
+    console.log(i)
+    
+  }
+
+  // Exercise 2 //
+  for (let i = 0; i <= 8; i++) {
+    console.log(8)
+  }
+
+  //Exercise 3 //
+let numb = Number(prompt('write a number'))
+for (let i = 1; i <= numb; i++) {
+    console.log ('hi' , i)
+}
